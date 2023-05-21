8b9e7e8d705ee7984a3976ed9a224a90d7652a82
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-21 23:45:46 +0300

    [Array manipulation down, prints figures]

--- main.zig
+++ main.zig
@@ -1,5 +1,18 @@
 const std = @import("std");
 
 pub fn main() void{
-    std.debug.print("Hello, {s}!", .{"Roses"});
+    std.debug.print("Hello, {s}!", .{"Roses. \n"});
+
+    const travellers = [7]u8{'h', 'e', 'l', 'l', 'o', 'L', 'P'};
+    const travellersb = [_]u8{ 'a','k' };
+
+     std.debug.print("travellerslen: {}\n", .{travellers.len});
+          std.debug.print("travellers_b_len: {}\n", .{travellers.len});
+
+
+     for (travellers) |elem| {
+        std.debug.print("elem: {}\n", .{elem});
+    }
+
+ std.debug.print("concat: {any}\n", .{travellers ++ travellersb});
 }
